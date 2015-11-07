ea0bdf0b5aca34051b062668c296574b4764bd35
Alexander Pliskin <alexnetmore@gmail.com>
2015-11-08 01:47:32 +0300

    [E2015-128] time in normal format

--- Test My Social Network/src/main/java/com/pliskin/aop/ServiceAspect.java
+++ Test My Social Network/src/main/java/com/pliskin/aop/ServiceAspect.java
@@ -4,9 +4,13 @@ package com.pliskin.aop;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 public class ServiceAspect {
+
+    private DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
     public Object timeLog(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
@@ -15,12 +19,12 @@ public class ServiceAspect {
                 + " с параметрами" + Arrays.toString(joinPoint.getArgs())
                 + " в классе : "
                 + joinPoint.getTarget().getClass().getSimpleName()
-                + " время: " + start);
+                + " время: " + df.format(start));
         Object result = joinPoint.proceed();
         long end = System.currentTimeMillis();
         System.out.println("Звершение выполнения метода: "
-                + joinPoint.getSignature().getName() + " время: " + end + "");
-        System.out.println("Время выполнения: " + (end - start));
+                + joinPoint.getSignature().getName() + " время: " + df.format(end) + "");
+        System.out.println("Время выполнения: " + (end - start) + " ms");
         return result;
     }
 
